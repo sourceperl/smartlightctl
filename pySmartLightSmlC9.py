ddcc0c11001267edeb8aaa4e166edd27ba57f486
sourceperl <loic.celine@free.fr>
2016-09-04 22:35:39 +0200

    add RGB support

--- pySmartLightSmlC9.py
+++ pySmartLightSmlC9.py
@@ -7,14 +7,19 @@ class SmartLight(btle.Peripheral):
         btle.Peripheral.__init__(self, addr)
         self.delegate = NotificationDelegate()
         self.setDelegate(self.delegate)
-        self.light_svc = self.getServiceByUUID('33160fb9-5b27-4e70-b0f8-ff411e3ae078')
-        self.light_cmd_ch = self.light_svc.getCharacteristics('217887f8-0af2-4002-9c05-24c9ecf71600')[0]
+        self.svc_cmd = self.getServiceByUUID('33160fb9-5b27-4e70-b0f8-ff411e3ae078')
+        self.light_cmd_ch = self.svc_cmd.getCharacteristics('217887f8-0af2-4002-9c05-24c9ecf71600')[0]
+        self.svc_rgb = self.getServiceByUUID('b882e31b-5096-43d1-90a9-edbf95073337')
+        self.light_rgb_ch = self.svc_rgb.getCharacteristics('74532143-fff1-460d-8e8a-370f934d40be')[0]
 
     def on(self):
         self.light_cmd_ch.write(binascii.unhexlify('01'))
 
     def off(self):
         self.light_cmd_ch.write(binascii.unhexlify('00'))
+
+    def set_rgb(self, rgb):
+        self.light_rgb_ch.write(binascii.unhexlify(rgb))
 
 
 class NotificationDelegate(btle.DefaultDelegate):
